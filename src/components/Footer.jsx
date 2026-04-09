bd08bd9ef7da0fad7f63ce18d8188f0c74866907
sherinesivy <irshathsherine@gmail.com>
2026-04-09 05:37:56 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ function Footer() {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>© 2026 The Pink Attic. Made with 🌸</p>
+        <p>© 2026 The Pink Attic.</p>
       </div>
     </footer>
   );
